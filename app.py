8bf38b4a6c2a9ad5b8468dc37ee02d2acccd8b6f
周宇盛 <zys@xingyunbooks.com>
2017-10-04 03:00:55 +0800

    首页展示播放链接的列表

--- app.py
+++ app.py
@@ -5,17 +5,14 @@ from recursionFile import scanpath
 app = flask.Flask(__name__)
 
 
+# TODO
+# 首页展示播放列表，详情页播放
+
 @app.route('/')
 def index():
     video_list = scanpath('static', 'mp4')
     print(video_list)
     return flask.render_template('index.html', video_list=video_list)
-
-
-@app.route('/videoList')
-def youtube():
-    video_list = scanpath('static', 'mkv')
-    return flask.jsonify(video_list)
 
 
 if __name__ == '__main__':
